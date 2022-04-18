bede7c626e9e0cece644b810558ab44fab6d2847
Gavin Kimpson <gkimpson@gmail.com>
2022-04-18 15:47:07 +0100

    use routing to make apps the proper way

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,20 @@
 require('./bootstrap');
 
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from "vue-router";
 import PostsIndex from './components/Posts/Index'
 import laravelVuePagination from 'laravel-vue-pagination';
 
+const routes = [
+    { path: '/', component: PostsIndex }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
 const app = createApp({})
-app.component('posts-index', PostsIndex)
+app.use(router);
 app.component('Pagination', laravelVuePagination)
 app.mount('#app')
