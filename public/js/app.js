f33bc8b3ab0e1e6d92e45334eada1412802584b2
Gavin Kimpson <gkimpson@gmail.com>
2022-04-20 17:08:37 +0100

    minor tailwind tweak to the view

--- public/js/app.js
+++ public/js/app.js
@@ -21977,7 +21977,7 @@ var _hoisted_13 = {
   "class": "py-12"
 };
 var _hoisted_14 = {
-  "class": "w-full sm:px-6 lg:px-8"
+  "class": "max-w-7xl mx-auto sm:px-6 lg:px-8"
 };
 var _hoisted_15 = {
   "class": "bg-white overflow-hidden shadow-sm sm:rounded-lg"
